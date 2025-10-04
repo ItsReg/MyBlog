88879d26d7b3ba154101c7d4a709125caf754ef0
Sandeep Srivastava <sandytechexpert@gmail.com>
2025-10-04 20:16:21 +0530

    Updated base to handle 404 clicks

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 	// site: siteConfig.site,
-	site: 'https://itsreg.github.io/',
+	site: 'https://itsreg.github.io/MyBlog',
 	// base:'/MyBlog',
 	output: 'static',
 	markdown: {
